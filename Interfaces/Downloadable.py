bc0c16d63b19ffe4d468dcda5ab224013abe23fa
Edward Walsh <edwardjwalsh@protonmail.com>
2022-03-12 16:50:24 -0500

    wnba

--- Interfaces/Downloadable.py
+++ Interfaces/Downloadable.py
@@ -10,17 +10,33 @@ from urllib.request import urlopen, Request
 from pprint import pprint
 
 
-headers = {
-            "Host": "sports.yahoo.com",
-            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "none",
-            "Sec-Fetch-User": "?1",
-            "Sec-GPC": "1",
-            "Cache-Control": "max-age=0",
-            "TE": "trailers"
-        }
+def detachedDownload(url, attempts = 3, sleepTime = 10):
+    """
+    Recursive function to download yahoo url and isolate json
+    Or write to errorFile
+    """
+
+    item = {}
+
+    try:
+        print("New Download\n{}\n".format(url))
+        html = urlopen(url)
+        for line in [x.decode("utf-8") for x in html.readlines()]:
+            if "root.App.main" in line:
+                item = json.loads(";".join(line.split("root.App.main = ")[1].split(";")[:-1]))
+                item = item["context"]["dispatcher"]["stores"]
+
+    except (URLError, HTTPError, ValueError) as e:
+        if hasattr(e,'code'):
+            print (e.code)
+        if hasattr(e,'reason'):
+            print (e.reason)
+        if attempts:
+            sleep(sleepTime)
+            return self.downloadItem(attempts -1, sleepTime)
+        pass
+    # pprint(item)
+    return item
 
 
 ################################################################################
